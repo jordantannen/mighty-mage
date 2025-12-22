6ed5bbc91f08939af4f0d1668313d53abf5c5640
Jordan Tannenbaum <jtannenbaum3@gatech.edu>
2025-12-22 12:26:02 -0800

    Add basic player controller

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 0;
+    
+    private Rigidbody rb; 
+    private float movementX;
+    private float movementY;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x; 
+        movementY = movementVector.y; 
+    }
+    
+    private void FixedUpdate() 
+    {
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        rb.linearVelocity= movement * speed; 
+    }
+}
+
