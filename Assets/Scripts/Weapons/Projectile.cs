b4f434b7af37151e2633fcf91bb7cf21b53d31f5
Jordan Tannenbaum <jtannenbaum3@gatech.edu>
2025-12-27 23:43:29 -0500

    Complete Projectile

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -1,17 +1,56 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
 public class Projectile : MonoBehaviour
 {
-    [SerializeField] private float m_maxLifetime = 5f;
+    [SerializeField] private float m_maxLifetime = 5f; // In seconds
     
     private int m_damage;
     private float m_speed;
-    private Transform m_direction;
+    private Vector3 m_direction;
     private GameObjectPool m_pool;
+    private Rigidbody m_rigidbody;
 
-    public void Initialize()
+    public void Fire(int damage, float speed, Vector3 direction, GameObjectPool pool)
     {
+        m_damage = damage;
+        m_speed = speed;
+        m_direction = direction;
+        m_pool = pool;
         
+        StopAllCoroutines();
+        StartCoroutine(ReturnAfterLifetime());
+
+        m_rigidbody.linearVelocity = m_direction.normalized * m_speed;
+    }
+
+    private void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Enemy>(out Enemy enemy))
+        {
+            enemy.GetComponent<HealthHandler>().TakeDamage(m_damage);
+            ReturnToPool();
+        }
+    }
+    
+    private void ReturnToPool()
+    {
+        m_rigidbody.linearVelocity = Vector3.zero;
+        m_pool.Return(gameObject);
+    }
+    
+    private IEnumerator ReturnAfterLifetime()
+    {
+        yield return new WaitForSeconds(m_maxLifetime);
+        ReturnToPool();
     }
 }
 
